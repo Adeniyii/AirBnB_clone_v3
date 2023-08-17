edde51ed38eb14eec8fa6586ad8043caccc24eb1
Adeniyii <ifedayoadeniyi@gmail.com>
2023-08-17 07:57:02 +0100

    feat: implement routes for an amenity entity

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,7 +1,8 @@
-""""""
+"""Initializes main blueprint for flask application"""
 from flask import Blueprint
 
 app_views = Blueprint("site", __name__)
 from api.v1.views.index import *  # noqa
 from api.v1.views.states import *  # noqa
 from api.v1.views.cities import *  # noqa
+from api.v1.views.amenities import *  # noqa
