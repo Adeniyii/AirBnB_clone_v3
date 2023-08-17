cd094e08e488595cd016ee2b068be7bc67a72b3b
Adeniyii <ifedayoadeniyi@gmail.com>
2023-08-17 12:37:24 +0100

    feat: enable cross origin resource sharing on all domains

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,9 +5,11 @@ from flask import Flask
 from flask.json import jsonify
 from models import storage
 from api.v1.views import app_views
-
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
+
 app.register_blueprint(app_views, url_prefix="/api/v1")
 
 
